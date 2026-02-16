e20688e95c61f084f989099167d10582c1d6645a
Lucas Xavier <lucas.xavier@externo.cresol.com.br>
2026-02-16 19:51:43 -0300

    fix: change `resolve` to `join` in `api/v1/migrations.js`

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,5 +1,5 @@
 import { runner } from 'node-pg-migrate';
-import { resolve } from 'node:path';
+import { join } from 'node:path';
 
 import database from 'infra/database';
 
@@ -17,7 +17,7 @@ export default async function migrations(request, response) {
     const defaultMigartionsOptions = {
       dbClient: dbClient,
       dryRun: true,
-      dir: resolve('infra', 'migrations'),
+      dir: join('infra', 'migrations'),
       direction: 'up',
       verbose: true,
       migrationsTable: 'pgmigrations',
